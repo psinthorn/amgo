794d2f5882e47588a79ca78403ca712be01bb7c7
ecosyn <psinthorn@gmail.com>
2018-10-28 19:51:11 +0700

    Anonymouse struct

---
+++ sec-12/ex-04.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//anonymouse struct
+	anm := struct {
+		fname string
+		lname string
+		//use map[key type]value type as field
+		phone map[string]int
+
+		//use slide as filed
+		favFoods []string
+	}{
+		fname: "Sinthorn",
+		lname: "Pr.",
+		phone: map[string]int{
+			"Nut":    555,
+			"Phansa": 333,
+		},
+		favFoods: []string{
+			"Padthai",
+			"Noodle",
+			"Soup",
+			"Papaya Salad",
+		},
+	}
+
+	fmt.Println(anm)
+
+	//loop map with for->range key: value
+	for key, value := range anm.phone {
+		fmt.Println(key, value)
+	}
+
+	//loop slide composit literal by for->range
+	for index, value := range anm.favFoods {
+		fmt.Println(index, value)
+	}
+
+}
